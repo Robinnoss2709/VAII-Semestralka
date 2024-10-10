bd23728733005ffda81beaa874fe4109c791e5a6
Robinnoss27 <robinnoss27@gmail.com>
2024-10-10 20:17:25 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
+  </script>
+  
+  <div class="relative flex flex-col  mx-auto w-full text-sm sm:text-base min-h-screen">
+    
+    <Header/>
+    <slot />
+    <Footer/>
+  </div>
